eff443b4af7b4663e4d330830659a2f2640a47fb
Alterfero <alterfero@gmail.com>
2025-08-27 14:42:16 -1000

    Fix syntax errors in config.inc.php

--- web/config.inc.php
+++ web/config.inc.php
@@ -128,6 +128,9 @@ $default_language_tokens = "fr";
 $timezone = 'Pacific/Honolulu';
 date_default_timezone_set($timezone);
 setlocale(LC_ALL, 'fr_FR.UTF-8');
-$weekstarts = 1;
+$weekstarts = 1
+$view_week_number = true:
+$default_view = "month";
+
 
 
